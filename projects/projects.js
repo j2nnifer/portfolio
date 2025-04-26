b3e6aa1d4c478d0db91c8864e48a47b23b304337
j2nnifer <jekuei@ucsd.edu>
2025-04-25 17:22:32 -0700

    idkanymore

--- projects/projects.js
+++ projects/projects.js
@@ -2,5 +2,12 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 
 const projects = await fetchJSON('../lib/projects.json');
+
 const projectsContainer = document.querySelector('.projects');
+
 renderProjects(projects, projectsContainer, 'h2');
+
+const titleElement = document.querySelector('.projects-title');
+    if (titleElement) {
+      titleElement.textContent = `${projects.length} Projects`;
+    }
