ee3a041bc2666efbd8022825c11e7e1deacaf0bb
j2nnifer <jekuei@ucsd.edu>
2025-04-25 17:00:49 -0700

    maybe part3 lab4

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ if (latestProjects && Array.isArray(latestProjects) && projectsContainer)
     {
     renderProjects(latestProjects, projectsContainer, 'h2');
     }
+
+const githubData = await fetchGitHubData('giorgianicolaou');
+const profileStats = document.querySelector('#profile-stats');
+
+
+if (profileStats) {
+    profileStats.innerHTML = `
+          <dl>
+            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers:</dt><dd>${githubData.followers}</dd>
+            <dt>Following:</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+  }
+  
