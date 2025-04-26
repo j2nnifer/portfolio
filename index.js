0aebbb77005a03cfdded91cc4030c7b582437061
j2nnifer <jekuei@ucsd.edu>
2025-04-25 17:26:06 -0700

    aaaaaaaaaaaaa

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 
 
+async function displayLatestProjects() {
+    const projectsContainer = document.querySelector('.projects');
+  
+    const projects = await fetchJSON('./lib/projects.json');
+    const latestProjects = projects.slice(0, 3);
+  
+    renderProjects(latestProjects, projectsContainer, 'h2');
+  }
 
-const projects = await fetchJSON('./lib/projects.json');
-const latestProjects = projects.slice(0, 3);
-const projectsContainer = document.querySelector('.projects');
 
-renderProjects(latestProjects, projectsContainer, 'h2');
-
-const githubData = await fetchGitHubData('giorgianicolaou');
-const profileStats = document.querySelector('#profile-stats');
-
-
-if (profileStats) {
-    profileStats.innerHTML = `
-          <dl>
-            <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-            <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-            <dt>Followers:</dt><dd>${githubData.followers}</dd>
-            <dt>Following:</dt><dd>${githubData.following}</dd>
-          </dl>
+  async function displayGithubStats() {
+    const profileStats = document.querySelector('#profile-stats');
+    if (profileStats) {
+      const githubData = await fetchGitHubData('j2nnifer');
+      profileStats.innerHTML = `
+        <h3>My GitHub Stats</h3>
+        <div class="github-stats-grid">
+          <div class="stat-item">
+            <div class="stat-label">Followers</div>
+            <div class="stat-value">${githubData.followers}</div>
+          </div>
+          <div class="stat-item">
+            <div class="stat-label">Following</div>
+            <div class="stat-value">${githubData.following}</div>
+          </div>
+          <div class="stat-item">
+            <div class="stat-label">Public Repos</div>
+            <div class="stat-value">${githubData.public_repos}</div>
+          </div>
+          <div class="stat-item">
+            <div class="stat-label">Public Gists</div>
+            <div class="stat-value">${githubData.public_gists}</div>
+          </div>
+        </div>
       `;
+    }
   }
   
+
+  displayGithubStats();
+  displayLatestProjects();
