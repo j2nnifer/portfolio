b273a525ede8a66acdfb4341bc2d270b3cbddb26
j2nnifer <jekuei@ucsd.edu>
2025-05-08 00:54:51 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,9 @@ import { fetchJSON, renderProjects } from '../global.js';
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 
-const projects = await fetchJSON('../lib/projects.json');
+const myprojects = await fetchJSON('../lib/projects.json');
+let projects = myprojects // fetch your project data
+
 
 const projectsContainer = document.querySelector('.projects');
 
@@ -19,14 +21,16 @@ let svg = d3.select('svg');
 
 
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year,
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
+
 let total = 0;
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
@@ -60,6 +64,7 @@ data.forEach((d, idx) => {
     .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
     .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
+
 
 
 
