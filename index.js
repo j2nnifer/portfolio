576311eee093b2f91fe1184e505d0bbbf7fa3508
j2nnifer <jekuei@ucsd.edu>
2025-05-08 01:16:05 -0700

    I FORGOT TO SAVE SO IT DIDNT UPDATE WTFF

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-import {    fetchJSON, renderProjects, fetchGithubData } from './global.js';'
-
-import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-
+import {    fetchJSON, renderProjects, fetchGithubData } from './global.js';
 
 
 async function latestprojs() {
@@ -46,8 +43,4 @@ async function ghstats() {
   ghstats();
   latestprojs();
 
-  let arc = d3.arc().innerRadius(0).outerRadius(50)({
-    startAngle: 0,
-    endAngle: 2 * Math.PI,
-  });
   
