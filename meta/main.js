f6f757fc5d6eb6418fbf8234970001319726699c
j2nnifer <jekuei@ucsd.edu>
2025-05-09 21:19:59 -0700

    plot?

--- meta/main.js
+++ meta/main.js
@@ -46,7 +46,7 @@ function processCommits(data) {
     const dl = d3.select('#stats').append('dl').attr('class', 'stats');
   
     // Total lines of code
-    dl.append('dt').html('Total Lines');
+    dl.append('dt').html('Total Lines of Code');
     dl.append('dd').text(data.length);
   
     // Total commits
@@ -85,6 +85,38 @@ function processCommits(data) {
     dl.append('dt').text('Most work done');
     dl.append('dd').text(mostCommonTime);
   }
+
+  function renderScatterPlot(data, commits) {
+    // Put all the JS code of Steps inside this function
+    const width = 1000;
+    const height = 600;
+
+    const svg = d3
+        .select('#chart')
+        .append('svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+        .style('overflow', 'visible');
+
+    const xScale = d3
+        .scaleTime()
+        .domain(d3.extent(commits, (d) => d.datetime))
+        .range([0, width])
+        .nice();
+
+    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+    const dots = svg.append('g').attr('class', 'dots');
+
+    dots
+        .selectAll('circle')
+        .data(commits)
+        .join('circle')
+        .attr('cx', (d) => xScale(d.datetime))
+        .attr('cy', (d) => yScale(d.hourFrac))
+        .attr('r', 5)
+        .attr('fill', 'steelblue');
+
+   }
 
 let data = await loadData();
 let commits = processCommits(data);
