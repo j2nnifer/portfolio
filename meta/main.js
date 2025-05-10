8b75e9e82bc8336138cb6b135f8cf0a9897b6b53
j2nnifer <jekuei@ucsd.edu>
2025-05-09 21:50:02 -0700

    upd

--- meta/main.js
+++ meta/main.js
@@ -116,6 +116,15 @@ function processCommits(data) {
         .attr('r', 5)
         .attr('fill', 'steelblue');
 
+    // Add gridlines BEFORE the axes
+    const gridlines = svg
+        .append('g')
+        .attr('class', 'gridlines')
+        .attr('transform', `translate(${usableArea.left}, 0)`);
+
+    // Create gridlines as an axis with no labels and full-width ticks
+gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
     const margin = { top: 10, right: 10, bottom: 30, left: 20 };
 
     const usableArea = {
